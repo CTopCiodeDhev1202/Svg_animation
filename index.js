f95fbc742e6c1f384d7a0ad4bd23f336046c5123
TopCodeDev <TopCodeDev1202@outlook.com>
2021-09-03 18:44:27 +0900

    second commit

--- index.js
+++ index.js
@@ -4,23 +4,16 @@ var randomRange = function(min, max) {
   return ~~(Math.random() * (max - min + 1)) + min
 };
 
-var numberOfLines = 2,
+var numberOfLines = 41,
   lineDataArr = [];
 
 var createPathString = function() {
 
-  var completedPath = '',
-    comma = ',',
-    ampl = 50; // pixel range from 0, aka how deeply they bend
+  var completedPath = ''; // pixel range from 0, aka how deeply they bend
 
   for (var i = 0; i < numberOfLines; i++) {
 
     var path = lineDataArr[i];
-
-    var current = {
-      x: ampl * Math.sin(path.counter / path.sin),
-      y: ampl * Math.cos(path.counter / path.cos)
-    };
 
     var newPathSection = 'M' +
       // starting point
@@ -28,17 +21,7 @@ var createPathString = function() {
       comma +
       path.startY +
       // quadratic control point
-      ' Q' +
-      path.pointX +
-      comma +
-      (current.y * 1.5).toFixed(3) + // 1.5 to amp up the bend a little
-      // center point intersection
-      ' ' +
-      ((current.x) / 10 + path.centerX).toFixed(3) +
-      comma +
-      ((current.y) / 5 + path.centerY).toFixed(3) +
-      // end point with quadratic reflection (T) (so the bottom right mirrors the top left)
-      ' T' +
+      ' L' +
       path.endX +
       comma +
       path.endY;
@@ -54,26 +37,16 @@ var createPathString = function() {
 
 var createLines = function() {
 
-  var newPathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path'),
-    // higher is slower
-    minSpeed = 85,
-    maxSpeed = 85;
-
-    console.log("before", newPathEl);
+  var newPathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
   // create an arr which contains objects for all lines
   // createPathString() will use this array
   for (var i = 0; i < numberOfLines; i++) {
     var lineDataObj = {
       counter: randomRange(1, 500), // a broad counter range ensures lines start at different cycles (will look more random)
-      startX: randomRange(-5, -40),
-      startY: randomRange(-5, -30),
-      endX: randomRange(200, 220), // viewbox = 200
-      endY: randomRange(120, 140), // viewbox = 120
-      sin: randomRange(minSpeed, maxSpeed),
-      cos: randomRange(minSpeed, maxSpeed),
-      pointX: randomRange(30, 55),
-      centerX: randomRange(90, 120),
-      centerY: randomRange(60, 70)
+      startX: 20 + 6 * i - i * (i + 1) * 0.14 / 2,
+      startY: 5,
+      endX: 180 - 1 * i - i * (i + 1) * 0.13 / 2, // viewbox = 200
+      endY: 190
     }
 
     lineDataArr.push(lineDataObj)
