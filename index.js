4bc7307413a479f8d0068dbdb89f8bf9a8b1c395
TopCodeDev <TopCodeDev1202@outlook.com>
2021-09-03 21:04:57 +0900

    load

--- index.js
+++ index.js
@@ -4,32 +4,52 @@ var randomRange = function(min, max) {
   return ~~(Math.random() * (max - min + 1)) + min
 };
 
-var numberOfLines = 41,
+var numberOfLines = 41, index = 0,
   lineDataArr = [];
 
 var createPathString = function() {
 
-  var completedPath = ''; // pixel range from 0, aka how deeply they bend
+  index++;
 
-  for (var i = 0; i < numberOfLines; i++) {
+  var completedPath = '', comma = ','; // pixel range from 0, aka how deeply they bend
 
-    var path = lineDataArr[i];
+  var path = lineDataArr[index];
 
-    var newPathSection = 'M' +
+  if (index < 41) {
+    for (var i = 0; i < numberOfLines; i++){    
+      starty = path.startY - 4 * i + i * (i + 1) * 0.1 / 2;
+      endy = path.endY + index * 0.3 * i - i * (i + 1) * index * 0.3 / 80;
+      var newPathSection = 'M' +
       // starting point
       path.startX +
       comma +
-      path.startY +
+      starty +
       // quadratic control point
       ' L' +
       path.endX +
       comma +
+      endy;
+  
+      completedPath += newPathSection;
+    }
+  }else{
+    for (var i = 0; i < numberOfLines; i++){    
+      startx = path.startX + index * 0.1 * i - i * (i + 1) * index * 0.1 / 80;
+      endx = path.endX - index * 0.05 * i - i * (i + 1) * index * 0.05 / 80;
+      var newPathSection = 'M' +
+      // starting point
+      startx +
+      comma +
+      path.startY +
+      // quadratic control point
+      ' L' +
+      endx +
+      comma +
       path.endY;
-    path.counter++;
-
-    completedPath += newPathSection;
-
-  };
+  
+      completedPath += newPathSection;
+    }
+  }
 
   return completedPath;
 
@@ -40,27 +60,36 @@ var createLines = function() {
   var newPathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path');
   // create an arr which contains objects for all lines
   // createPathString() will use this array
-  for (var i = 0; i < numberOfLines; i++) {
+  for (var i = 0; i < 41; i++) {
     var lineDataObj = {
-      counter: randomRange(1, 500), // a broad counter range ensures lines start at different cycles (will look more random)
-      startX: 20 + 6 * i - i * (i + 1) * 0.14 / 2,
-      startY: 5,
-      endX: 180 - 1 * i - i * (i + 1) * 0.13 / 2, // viewbox = 200
-      endY: 190
+      startX: 20,
+      startY: 100 - i * 2,
+      endX: 280, // viewbox = 200
+      endY: 100 + i * 2
+    }
+
+    lineDataArr.push(lineDataObj)
+  }
+
+  for (var i = 0; i < 41; i++) {
+    var lineDataObj = {
+      startX: 25 + 2.5 * i,
+      startY: 20,
+      endX: 275 - 3.5 * i, // viewbox = 200
+      endY: 180
     }
 
     lineDataArr.push(lineDataObj)
   }
 
   var animLoop = function() {
-    
     newPathEl.setAttribute('d', createPathString());
-    console.log("after", newPathEl);
-    //requestAnimationFrame(animLoop);
+    if (index < 82) requestAnimationFrame(animLoop);
   }
 
   // once the path elements are created, start the animation loop
   svgEl.appendChild(newPathEl);
+  console.log(lineDataArr);
   animLoop();
 
 };
